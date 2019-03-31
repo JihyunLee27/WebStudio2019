1c2b9834867447d1dcb10d3c2e8f2294f88a3700
이지현 <JihyunLee@ijihyeon-ui-MacBook-Pro.local>
2019-04-01 02:11:50 +0900

    Task3 Completed

--- 4_flask_restful/practice/JihyunLee/answer.py
+++ 4_flask_restful/practice/JihyunLee/answer.py
@@ -158,8 +158,78 @@ class ArticleList(Resource):
         return '{} does not exist'.format(_id)
 
 
+class CommentList(Resource):
+    filename = 'comments.json'
+
+    def get_comments(self):
+        comments = []
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r') as fp:
+                comments = json.loads(fp.read())
+        return comments  
+
+    def get(self):
+        if not os.path.exists(self.filename):
+            return 'comments.json does not exist'
+        r = self.get_comments()
+        s = ''
+        for d in r:
+            id = d['id']
+            user_id = d['user_id']
+            article_id = d['article_id']
+            content = d['content']
+            s += '[id: {}, user_id: {}, article_id: {}, content: {}]'.format(id, user_id, article_id, content)
+        return s
+
+    def post(self):
+        r_json = request.get_json()
+        user_id = r_json['user_id']
+        article_id = r_json['article_id']
+        content = r_json['content']
+        r = self.get_comments()
+        _id = 0
+        for d in r:
+            _id = max(_id, d['id'])
+        _id = _id +1
+        r_json['id'] = _id
+        r. append(r_json)
+        with open(self.filename, 'w') as fp:
+            fp.write(json.dumps(r))
+        return 'user_id: {}, article_id: {}, content: {}'. format(user_id, article_id, content)
+       
+    def put(self):
+        r_json = request.get_json()
+        _id = r_json['id']
+        content = r_json['content']
+        comments = self.get_comments()
+        found = False
+        for idx, _ in enumerate(comments):
+            if comments[idx]['id'] == _id:
+                found = True
+                comments[idx]['content'] =content
+                with open(self.filename, 'w') as fp:
+                    fp.write(json.dumps(comments))
+                return 'Comments Edited successfully'
+        return '{} does not exist'.format(_id)
+
+    def delete(self):
+        r_json = request.get_json()
+        _id = r_json['id']
+        comments = self.get_comments()
+        found = False
+        for idx, _ in enumerate(comments):
+            if comments[idx]['id'] == _id:
+                found = True
+                del comments[idx]
+                with open(self.filename, 'w') as fp:
+                    fp.write(json.dumps(comments))
+                return 'Deleted successfully'
+        return '{} does not exist'.format(_id)
+         
+    
 api.add_resource(UserList, '/api/users')
 api.add_resource(ArticleList, '/api/articles')
+api.add_resource(CommentList, '/api/comments')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
